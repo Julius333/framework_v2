b82972fa91d8f8cbb1fb40603658c9e3da773209
Julius333 <julius_nuijten@hotmail.com>
2017-06-21 23:11:53 +0200

    al het commentaar

--- framework/js/index.js
+++ framework/js/index.js
@@ -1,14 +1,25 @@
-	var accItem = document.getElementsByClassName('accordionItem');
-    var accHD = document.getElementsByClassName('accordionItemHeading');
-    for (i = 0; i < accHD.length; i++) {
+	
+// Variabelen aanmaken die gekoppeld worden aan elementen in HTML d.m.v. classe naam
+var accItem = document.getElementsByClassName('accordionItem');
+var accHD = document.getElementsByClassName('accordionItemHeading');
+
+
+// De length word bepaald afhankelijk van de grootte vand de titel 
+    for (i = 0; i < accHD.length; i++) 
+    {
         accHD[i].addEventListener('click', toggleItem, false);
     }
-    function toggleItem() {
-        var itemClass = this.parentNode.className;
-        for (i = 0; i < accItem.length; i++) {
+
+// Binnen deze functie wordt bepaald wanneer welke css transitie toegepast wordt.
+    function toggleItem() 
+        {
+            // De grootte van de div is afhankelijk van de grootte van de content die erbinnen staat
+            var itemClass = this.parentNode.className;
+            for (i = 0; i < accItem.length; i++) {
             accItem[i].className = 'accordionItem close';
         }
-        if (itemClass == 'accordionItem close') {
+        if (itemClass == 'accordionItem close') 
+        {
             this.parentNode.className = 'accordionItem open';
         }
     }
